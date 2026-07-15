303cc99a0701d9306a3695adca903d46b67f63d0
jyaul <mdjyaulhaque9934@gmail.com>
2026-07-15 13:53:11 +0530

    Remove node_modules from repository

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");
+
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -18,7 +21,7 @@ main()
     .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+  await mongoose.connect(process.env.MONGO_URL);
 }
 
 // Index Route
